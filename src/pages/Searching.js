995d84662115ca3c8d26d0020420068e5f7e9503
minhyuk2 <71816821+minhyuk2@users.noreply.github.com>
2024-01-03 13:06:03 +0900

    feat: searching ui 일부분 구현
    탐색 탭에 필요한 ui를 일부분 구현하였습니다.

--- src/pages/Searching.js
+++ src/pages/Searching.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React ,{useState} from 'react';
 
+import styled from 'styled-components';
+import { IoSearchSharp } from "react-icons/io5";
 const Searching = () => {
+    const [inputValue, setInputValue] = useState('');
+    
+    
+    
+    const changeInputValue = event => {
+        setInputValue(event.target.value)
+        
+    } 
+    
     return (
-        <div>
-            
-        </div>
+        <SearchingPackage>
+            <MusicText>
+                {"어떤 음악을 공유하고 싶으신가요?"}
+            </MusicText>
+            <form>
+                <label>
+                    <InputText type='text' placeholder='검색어를 입력해주세요' value = {inputValue} onChange={changeInputValue} />
+                    <IoSearchSharp />
+                </label>
+            </form>
+        </SearchingPackage>
     );
 };
 
+
 export default Searching;
+const SearchingPackage = styled.div`
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+`;
+
+
+const MusicText = styled.h1`
+  padding-bottom:30px;
+`;
+const InputText = styled.input`
+    width: 550px;
+    border: none; 
+    border-bottom: solid 2px #aaaa;
+`;
