55069f0a1c84a8764d6bef4caed748e09b60155a
sunny3631 <sunny363111@naver.com>
2024-02-08 17:32:35 +0900

    feat :: 더미데이터 수정
    더미데이터 수정

--- src/store/dummyStory.js
+++ src/store/dummyStory.js
@@ -1,22 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+import {ReactComponent as Image2} from '../assets/Image2.svg'
+
+const StyledImage2 = <Image2 width="92px" height="92px" />;
+
+const post_id = [
+    { day: 5, image: { component: StyledImage2, alt: 'Image 5' } },
+    { day: 24, image: { component: StyledImage2, alt: 'Image 24' } },
+
+    { day: 31, image: { component: StyledImage2, alt: 'Image 31' } },
+  ];
 
 
-import image1 from '../assets/PlaylistImg1.svg';
-import image2 from '../assets/PlaylistImg2.svg';
-import image3 from '../assets/PlaylistImg3.svg';
-
-const images = [
-  {
-    id: 1,
-    image: image1,
-  },
-  {
-    id: 2,
-    image: image2,
-  },
-  {
-    id: 3,
-    image: image3,
-  },
-];
-
-export default images;
+export default post_id;
