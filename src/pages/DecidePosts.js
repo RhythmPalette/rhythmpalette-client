feae547b8e0a15b05fb5666c307a98053c915a3b
minhyuk2 <71816821+minhyuk2@users.noreply.github.com>
2024-02-18 14:15:03 +0900

    feat : 이모티콘들 정렬완료
    이모티콘들을 정렬완료하였고 서버와의 연결해야 함

--- src/pages/DecidePosts.js
+++ src/pages/DecidePosts.js
@@ -1,23 +1,30 @@
 import React ,{useEffect, useState} from 'react';
 import styled from 'styled-components';
-import { ReactComponent as ExampleSquareImg } from '../assets/ExampleSquareImg.svg';
-import { ReactComponent as NothingImg} from '../assets/Emoticons/Nothing.svg';
-import { ReactComponent as ComplexImg} from '../assets/Emoticons/Complex.svg';
-import { ReactComponent as DepressImg} from '../assets/Emoticons/Depressed.svg';
-import { ReactComponent as GoodImg} from '../assets/Emoticons/Good.svg';
-import { ReactComponent as HappyImg} from '../assets/Emoticons/Happy.svg';
-import { ReactComponent as NoExpressImg} from '../assets/Emoticons/NoExpression.svg';
-import { ReactComponent as SadImg} from '../assets/Emoticons/Sad.svg';
-import { ReactComponent as ShyImg} from '../assets/Emoticons/Shy.svg';
-import { ReactComponent as SickImg} from '../assets/Emoticons/Sick.svg';
-import { ReactComponent as SquerkImg} from '../assets/Emoticons/Squerk.svg';
+import  {ReactComponent as ExampleSquareImg}  from '../assets/ExampleSquareImg.svg';
+import  NothingImg from '../assets/Emoticons/Nothing.svg';
+import  ComplexImg from '../assets/Emoticons/Complex.svg';
+import  DepressImg from '../assets/Emoticons/Depressed.svg';
+import  GoodImg from '../assets/Emoticons/Good.svg';
+import  HappyImg from '../assets/Emoticons/Happy.svg';
+import  NoExpressImg from '../assets/Emoticons/NoExpression.svg';
+import  SadImg from '../assets/Emoticons/Sad.svg';
+import  ShyImg from '../assets/Emoticons/Shy.svg';
+import  SickImg from '../assets/Emoticons/Sick.svg';
+import  SquerkImg from '../assets/Emoticons/Squerk.svg';
 
 const DecidePosts = () => {
     const [hashClicked,setHashClicked] = useState(false);
-    
-    
-    const HashTagBtnClicked = () => {
+    const [situation1, setSituation1] = useState("");
+    const [situation2, setSituation2] = useState("");
+    const [situation3, setSituation3] = useState("");
+
+    const HashTagBtnClicked = (event) => {
+        event.stopPropagation();
         setHashClicked(!hashClicked);
+
+    }
+    const CompleteBtnClicked = () =>{
+        
 
     }
     //여기에 클릭되었을 때 hashtag넣을 수 있는 div만들어두고 
@@ -34,17 +41,16 @@ const DecidePosts = () => {
             <InPutPost type = "text" placeholder='게시글 내용을 입력해주세요'>
             </InPutPost>
             <EmotionDiv>
-                <NothingImg/>
-                <ComplexImg/>
-                <DepressImg/>
-                <GoodImg/>
-                <HappyImg/>
-                <NoExpressImg/>
-                <NothingImg/>
-                <SadImg/>
-                <ShyImg/>
-                <SickImg/>
-                <SquerkImg/>
+                <ComplexImage src ={ComplexImg} alt="짜증"/>
+                <DepressImage src = {DepressImg} alt="우울"/>
+                <GoodImage src ={GoodImg} alt="기분좋음"/>
+                <HappyImage src = {HappyImg} alt="행복"/>
+                <NoExpressImage src={NoExpressImg} alt="무표정"/>
+                <NothingImage src = {NothingImg} alt="무념무상"/>
+                <SadImage src = {SadImg} alt="슬픔"/>
+                <ShyImage src = {ShyImg} alt="머쓱"/>
+                <SickImage src = {SickImg} alt="아픔"/>
+                <SquerkImage src={SquerkImg} alt="삐짐"/>
             </EmotionDiv>
 
 
@@ -57,15 +63,27 @@ const DecidePosts = () => {
                     <MentDiv>
                         {"3개까지 추가할 수 있어요"}
                     </MentDiv>
+                    <HashTagDiv>
+                        {"#"}
                     <FirstHash>
-                    </FirstHash>
+                     </FirstHash>   
+                    </HashTagDiv>
+                    <HashTagDiv>
+                         {"#"}   
                     <SecondHash>
                     </SecondHash>
+                    </HashTagDiv>
+                    <HashTagDiv>
+                         {"#"}   
                     <ThirdHash>
                     </ThirdHash>
+                    </HashTagDiv>
+                    <BtnDiv>
                     <CompleteBtn>
                         {"완료"}
                     </CompleteBtn>
+                    </BtnDiv>
+                  
                 </WriteHash>
             )}
             </HashDiv>
@@ -77,12 +95,126 @@ const DecidePosts = () => {
 };
 
 export default DecidePosts;
+const SickImage = styled.img`
+width: 57.26px;
+height: 74.61px;
+top: 756.95px;
+left: 1075.62px;
+position: absolute;
+
+`;
+const ShyImage = styled.img`
+width: 60.11px;
+height: 75.21px;
+top: 554.66px;
+left: 1007.59px;
+position: absolute;
+
+
+`;
+const SadImage = styled.img`
+width: 57.19px;
+height: 72.48px;
+top: 636.06px;
+left: 1254.64px;
+position: absolute;
+
+
+
+`;
+const NothingImage = styled.img`
+width: 57.19px;
+height: 74.41px;
+top: 709.21px;
+left: 1008.79px;
+position: absolute;
+`;
+
+const HappyImage = styled.img`
+width: 57.19px;
+height: 74.41px;
+top: 709.21px;
+left: 1217.64px;
+position: absolute;
+
+
+`;
+
+const GoodImage = styled.img`
+width: 57.19px;
+height: 74.05px;
+top: 554.06px;
+left: 1223.61px;
+position: absolute;
+
+`;
+
+const ComplexImage = styled.img`
+width: 72.47px;
+height: 75.35px;
+top: 758.15px;
+left: 1149.61px;
+position: absolute;
+
+`;
+
+
+const DepressImage = styled.img`
+width: 57.19px;
+height: 73.85px;
+top: 632.24px;
+left: 978.95px;
+position: relative;
+`;
+
+const SquerkImage = styled.img`
+width: 57.19px;
+height: 73.52px;
+top: 510.5px;
+left: 1156.78px;
+position : absolute;
+`;
+
+
+const NoExpressImage = styled.img`
+width: 57.19px;
+height: 73.72px;
+top: 510.5px;
+left: 1075.55px;
+position: absolute;
+`;
+
+const HashTagDiv = styled.div`
+    border-bottom : 1px solid
+`;
+
+
+const BtnDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
 
 const EmotionDiv = styled.div`
+
 
 `;
 
 const CompleteBtn = styled.button`
+width: 117.64px;
+height: 38.54px;
+border-radius: 15px;
+margin-top: 10px;
+font-family: Pretendard Variable;
+font-size: 25px;
+font-weight: 400;
+line-height: 41px;
+letter-spacing: -1px;
+text-align: center;
+padding-bottom: 3px;
+background: #F9C0C6;
+display: ${props => props.hashClicked ? "none" : "block" };
 
 `;
 
@@ -93,19 +225,37 @@ font-weight: 400;
 line-height: 40px;
 letter-spacing: 0em;
 text-align: left;
-
+padding-left: 20px;
 
 
 `;
 
 const FirstHash = styled.input`
-
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    height: 50px;
+    border-bottom : 0;
+    width: 300px;
+    padding-left: 10px;
 `;
 const SecondHash = styled.input`
-
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    height: 50px;
+    border-bottom : 0;
+    width: 300px;
+    padding-left: 10px;
 `;
 const ThirdHash = styled.input`
-
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    height: 50px;
+    border-bottom : 0;
+    width: 300px;
+    padding-left: 10px;
 `;
 
 
@@ -115,10 +265,12 @@ const WriteHash = styled.div`
     width: 349.81px;
     height: 252.26px;
     top: 528.53px;
-    left: 1480.05px;
+    left: 1478.05px;
     border-radius: 15px;
     position: absolute;
     border: 1px solid #000000;
+    background-color: #ffff;
+    color : #999999;
 
 `;
 
@@ -126,9 +278,6 @@ const HashDiv = styled.div`
 
 `;
 
-const HashTagDiv = styled.div`
-
-`;
 
 const UploadButton = styled.button`
 background: #04DB8F;
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled  from 'styled-components';
 import { dummy } from '../store/postExampleDummyData';
 import Post from '../components/Post';
 const Posts = () => {
+
+const [selectedFilter, setSelectedFilter] = useState('all');
+const handleFilterChange = (event) => {
+    setSelectedFilter(event.target.value);
+  };
+    //이제 post들을 가져와서 띄우는 형식으로 보여주면 된다. Post들을 가져오는 부분을 여기에 짜고 Post에 정보들을 넘겨줘서 map으로 띄워주면 된다.
     return (
         <BigBox>
+        <FilterDiv>
+       
+        <Filtering value={selectedFilter} onChange={handleFilterChange}>
+          <option value="">필터</option>
+          <option value="all">전체</option>
+          <option value="latest">최신순</option>
+          <option value="likes">좋아요순</option>
+        </Filtering>
+        
+        </FilterDiv>
+        
         <PostContainer>
         {
             dummy.results.map((item)=>{
@@ -23,6 +40,28 @@ const Posts = () => {
 
 
 export default Posts;
+const Filtering = styled.select`
+position: relative;
+top: 183.79px;
+left: 576.63px;
+border-radius: 16px;
+border: 1px;
+font-family: Pretendard Variable;
+font-size: 27px;
+font-weight: 400;
+line-height: 32px;
+letter-spacing: 0.01em;
+text-align: center;
+width: 126.05px;
+height: 49.26px;
+border: 1px solid #000000;
+`;
+
+
+const FilterDiv = styled.div`
+
+`;
+
 const BigBox = styled.div`
 
 `
